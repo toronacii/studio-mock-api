38d65428ffebcbac8f830d2ed471981d6da30392
Jose Toro <intive.jtoro@veritran.com>
2021-10-18 19:03:02 -0300

    Added marketplace templates endpoint

--- api/index.js
+++ api/index.js
@@ -4,16 +4,21 @@ const api = express.Router();
 api.get('/', (req, res) => res.json({ response: 'success!' }))
 
 api.get('/applicationgenerate/:id/generateAndPublish.json', ({ query }, res) => {
-  let response = () => require('./responses/generateAndPublish/index.json');
+  let response = () => require('./responses/generate-and-publish/index.json');
   if (query.mobile) {
-    response = () => require('./responses/generateAndPublish/mobile.json');
+    response = () => require('./responses/generate-and-publish/mobile.json');
   }
 
   if (query.error) {
-    response = () => require('./responses/generateAndPublish/error.json');
+    response = () => require('./responses/generate-and-publish/error.json');
   }
 
   res.json(response());
 });
+
+api.get('/VRTP/marketplace/extensions.json', ({ query }, res) => {
+  if (query.type === 'template') return res.json(require('./responses/marketplace-templates/index.json'));
+  return res.json({ response: 'You must send the query type=template' })
+})
 
 module.exports = api;
