af0389e89f9ad82cf165cfb36760fb3a7990558c
Jose Toro <intive.jtoro@veritran.com>
2021-10-19 12:06:28 -0300

    CORS added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const serverless = require("serverless-http");
+const cors = require('cors');
 const app = require('express')();
 const api = require('./api');
 
+app.use(cors());
 app.use(({ query: { delay = 0 } }, res, next) => setTimeout(next, delay * 1000));
 app.use(api);
 app.use((req, res) => res.status(404).json({ error: "Not Found"}));
